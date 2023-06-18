973eacd6b75fa0b7cf428ea50d5876fbe259eb9d
Gilson <gilsonfabio@gmail.com>
2023-06-18 19:33:31 -0300

    Alteração exportação arquivo CSV

--- src/controllers/DownloadsController.js
+++ src/controllers/DownloadsController.js
@@ -1,6 +1,4 @@
 const connection = require('../database/connection');
-const fs = require('fs');
-const sendmail = require('../database/sendmail')({silent: true})
 
 module.exports = {   
     async downTexto (request, response) {
@@ -8,70 +6,19 @@ module.exports = {
         let final = request.params.datInicial;
         let idOrg = request.params.orgId;
 
-        //console.log(inicio);
-        //console.log(final);
-        //console.log(idOrg);
+        let year = inicio.getFullYear();
+        let month = inicio.getMonth() ;
 
-        const compras = await connection('cmpParcelas')
-            .where('parVctParcela','>=', inicio)
-            .where('parVctParcela','<=', final)
-            .where('orgId', idOrg)
-            .join('compras', 'cmpId', 'cmpParcelas.parIdCompra')
-            .join('servidores', 'usrId', 'compras.cmpServidor')
-            .join('convenios', 'cnvId', 'compras.cmpConvenio')
+        const saldo = await connection('usrSaldo')
+            .join('servidores', 'usrCartao', 'usrSaldo.usrServ')
             .join('secretarias', 'secId', 'servidores.usrSecretaria')
             .join('orgadmin', 'orgId', 'secretarias.secOrgAdm')
-            .select(['cmpParcelas.*', 'compras.cmpEmissao', 'compras.cmpServidor', 'compras.cmpConvenio', 'servidores.usrMatricula', 'servidores.usrNome', 'convenios.cnvNomFantasia','secretarias.secOrgAdm', 'orgadmin.orgId']);
-        
-        var nome_arquivo = 'arquivo.txt'
-        //console.log(nome_arquivo);
-        var separacao = ';' 
-        for (let compra of compras) {
-            //console.log(`${compra['parIdCompra']}`)
-            const data = `${compra['usrMatricula']}` + ';' + `${compra['usrNome']}`  + ';' + `${compra['parVlrParcela']}` + ';'
-            fs.appendFileSync(nome_arquivo, data + '\n', (err) => {
-                if (err) throw err;
-                console.log('O arquivo foi criado!');
-            })
-        }        
-
-        pathToAttachment = `arquivo.txt`;
-        attachment = fs.readFileSync(pathToAttachment).toString('utf8')
-          
-        const emailUsuario = 'gilsonfabio@gmail.com';
-        const nomeUsuario = 'Gilson Fábio';
-        const link = '';
-        const emailEnvio = 'gilsonfabio@innvento.com.br';
-               
-        const apiKey = process.env.SENDGRID_API_KEY;
-        const sgMail = require('@sendgrid/mail')
-
-        sgMail.setApiKey(apiKey)
-        const msg = {
-            to: emailUsuario,
-            from: `${emailEnvio}`,
-            subject: 'Email para Relatorio Mensal',
-            text: `Email de relatorio mensal`,
-            attachments: [
-                {
-                  content: attachment,
-                  filename: "arquivo.txt",
-                  type: "text/plain",
-                  disposition: "attachment"
-                }
-            ],
-            html: `<p>Olá, ${nomeUsuario}, </br></p><p>Você solicitou o arquivo de movimentação mensal.</p></br>`, 
-        }
-        sgMail
-          .send(msg)
-          .then(() => {
-            //console.log('Email sent')
-          })
-        .catch((error) => {
-            //console.error(error)
-        })     
-        
-        //return response.json(user);  
+            .where('usrMes',month)
+            .where('usrAno',year)
+            .where('orgId', idOrg)
+            .select(['servidores.usrMatricula', 'servidores.usrNome','usrSaldo.usrVlrUsado',])
+       
+        return response.json(saldo);  
         
     }          
 };
